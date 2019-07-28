f858e4f2fed9368c9ac2d93d7399fbf4a112d08f
Adrian Lemes <ilegra.adrian@agiplan.com.br>
2019-07-28 15:26:28 -0300

    add cypress and docker

---
+++ cypress/integration/spec.ts
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('Firestarter', () => {
+
+  it('has a heading', () => {
+    cy.visit('http://localhost:4200');
+    cy.contains('Welcome to angular-boilerplate!');
+    // or...
+    cy.get('h1').should('contain', 'Welcome to angular-boilerplate!');
+
+  });
+
+});
